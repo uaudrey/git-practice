9bab63c27bf1d2f4c7f67e97ec0699f53c0f80f5
Maggie <mjtice0@gmail.com>
2022-09-23 11:56:17 -0600

    Submission of function a git-practice

--- function_a.py
+++ function_a.py
@@ -1,8 +1,14 @@
+from statistics import mode
+
+
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
-
+    for number in nums:   
+        return mode(nums)
+        
+    
+    
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
